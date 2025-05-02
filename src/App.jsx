67fb69546293672ddcab162f8360434a9781bc9c
kytan999 <tan_kok_yong@yahoo.com>
2025-05-02 11:18:58 +0800

    image missing solution

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="app-container">
         <div className="dashboard-header">
           <div className="dashboard-branding">
-            <img src="src/images/logo.png" alt="logo" className="dashboard-icon" />
+            <img src="./assets/logo.png" alt="logo" className="dashboard-icon" />
             <h1 className="dashboard-title">Finance Dashboard</h1>
           </div>
 
